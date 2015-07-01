4e34a452bf18d0eaba3da1cd0f849f9fe4b7f015
Yogr <danh425@gmail.com>
2015-06-30 23:21:29 -0400

    Initial commit of CardMaker
    This is the first CL submitted for CardMaker

---
+++ CardMaker/CardMaker/CreateSetForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace CardMaker
+{
+    partial class CreateSetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title_label = new System.Windows.Forms.Label();
+            this.SetName_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Create_btn = new System.Windows.Forms.Button();
+            this.Cancel_btn = new System.Windows.Forms.Button();
+            this.SetType_chooser = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Title_label
+            // 
+            this.Title_label.AutoSize = true;
+            this.Title_label.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Title_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title_label.Location = new System.Drawing.Point(21, 24);
+            this.Title_label.Name = "Title_label";
+            this.Title_label.Size = new System.Drawing.Size(156, 24);
+            this.Title_label.TabIndex = 0;
+            this.Title_label.Text = "Create a New Set";
+            // 
+            // SetName_text
+            // 
+            this.SetName_text.Location = new System.Drawing.Point(206, 58);
+            this.SetName_text.Name = "SetName_text";
+            this.SetName_text.Size = new System.Drawing.Size(362, 20);
+            this.SetName_text.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Set Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Create_btn
+            // 
+            this.Create_btn.Location = new System.Drawing.Point(261, 360);
+            this.Create_btn.Name = "Create_btn";
+            this.Create_btn.Size = new System.Drawing.Size(140, 57);
+            this.Create_btn.TabIndex = 3;
+            this.Create_btn.Text = "Create";
+            this.Create_btn.UseVisualStyleBackColor = true;
+            // 
+            // Cancel_btn
+            // 
+            this.Cancel_btn.BackColor = System.Drawing.Color.DarkRed;
+            this.Cancel_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Cancel_btn.Location = new System.Drawing.Point(261, 433);
+            this.Cancel_btn.Name = "Cancel_btn";
+            this.Cancel_btn.Size = new System.Drawing.Size(140, 23);
+            this.Cancel_btn.TabIndex = 4;
+            this.Cancel_btn.Text = "Cancel";
+            this.Cancel_btn.UseVisualStyleBackColor = false;
+            // 
+            // SetType_chooser
+            // 
+            this.SetType_chooser.FormattingEnabled = true;
+            this.SetType_chooser.Items.AddRange(new object[] {
+            "Core Set",
+            "Expansion Set",
+            "Other"});
+            this.SetType_chooser.Location = new System.Drawing.Point(206, 98);
+            this.SetType_chooser.Name = "SetType_chooser";
+            this.SetType_chooser.Size = new System.Drawing.Size(259, 21);
+            this.SetType_chooser.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Set Type";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 90);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CreateSetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(670, 468);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SetType_chooser);
+            this.Controls.Add(this.Cancel_btn);
+            this.Controls.Add(this.Create_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SetName_text);
+            this.Controls.Add(this.Title_label);
+            this.Name = "CreateSetForm";
+            this.Text = "CreateSetForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title_label;
+        private System.Windows.Forms.TextBox SetName_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Create_btn;
+        private System.Windows.Forms.Button Cancel_btn;
+        private System.Windows.Forms.ComboBox SetType_chooser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ CardMaker/CardMaker/MainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace CardMaker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CardMaker_logo = new System.Windows.Forms.PictureBox();
+            this.CreateSet_btn = new System.Windows.Forms.Button();
+            this.LoadSet_btn = new System.Windows.Forms.Button();
+            this.DeleteSet_btn = new System.Windows.Forms.Button();
+            this.DeleteCard_btn = new System.Windows.Forms.Button();
+            this.LoadCard_btn = new System.Windows.Forms.Button();
+            this.CreateCard_btn = new System.Windows.Forms.Button();
+            this.DeleteEffect_btn = new System.Windows.Forms.Button();
+            this.LoadEffect_btn = new System.Windows.Forms.Button();
+            this.CreateEffect_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CardMaker_logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CardMaker_logo
+            // 
+            this.CardMaker_logo.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.CardMaker_logo.Location = new System.Drawing.Point(267, 12);
+            this.CardMaker_logo.Name = "CardMaker_logo";
+            this.CardMaker_logo.Size = new System.Drawing.Size(453, 255);
+            this.CardMaker_logo.TabIndex = 0;
+            this.CardMaker_logo.TabStop = false;
+            // 
+            // CreateSet_btn
+            // 
+            this.CreateSet_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateSet_btn.Location = new System.Drawing.Point(30, 347);
+            this.CreateSet_btn.Name = "CreateSet_btn";
+            this.CreateSet_btn.Size = new System.Drawing.Size(258, 55);
+            this.CreateSet_btn.TabIndex = 1;
+            this.CreateSet_btn.Text = "Create a New Set";
+            this.CreateSet_btn.UseVisualStyleBackColor = true;
+            // 
+            // LoadSet_btn
+            // 
+            this.LoadSet_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadSet_btn.Location = new System.Drawing.Point(30, 408);
+            this.LoadSet_btn.Name = "LoadSet_btn";
+            this.LoadSet_btn.Size = new System.Drawing.Size(258, 53);
+            this.LoadSet_btn.TabIndex = 2;
+            this.LoadSet_btn.Text = "Load an Existing Set";
+            this.LoadSet_btn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteSet_btn
+            // 
+            this.DeleteSet_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteSet_btn.Location = new System.Drawing.Point(30, 467);
+            this.DeleteSet_btn.Name = "DeleteSet_btn";
+            this.DeleteSet_btn.Size = new System.Drawing.Size(258, 53);
+            this.DeleteSet_btn.TabIndex = 3;
+            this.DeleteSet_btn.Text = "Delete a Set";
+            this.DeleteSet_btn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteCard_btn
+            // 
+            this.DeleteCard_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteCard_btn.Location = new System.Drawing.Point(355, 467);
+            this.DeleteCard_btn.Name = "DeleteCard_btn";
+            this.DeleteCard_btn.Size = new System.Drawing.Size(283, 53);
+            this.DeleteCard_btn.TabIndex = 6;
+            this.DeleteCard_btn.Text = "Delete a Card";
+            this.DeleteCard_btn.UseVisualStyleBackColor = true;
+            // 
+            // LoadCard_btn
+            // 
+            this.LoadCard_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadCard_btn.Location = new System.Drawing.Point(355, 408);
+            this.LoadCard_btn.Name = "LoadCard_btn";
+            this.LoadCard_btn.Size = new System.Drawing.Size(283, 53);
+            this.LoadCard_btn.TabIndex = 5;
+            this.LoadCard_btn.Text = "Load an Existing Card";
+            this.LoadCard_btn.UseVisualStyleBackColor = true;
+            // 
+            // CreateCard_btn
+            // 
+            this.CreateCard_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateCard_btn.Location = new System.Drawing.Point(355, 347);
+            this.CreateCard_btn.Name = "CreateCard_btn";
+            this.CreateCard_btn.Size = new System.Drawing.Size(283, 55);
+            this.CreateCard_btn.TabIndex = 4;
+            this.CreateCard_btn.Text = "Create a New Card";
+            this.CreateCard_btn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteEffect_btn
+            // 
+            this.DeleteEffect_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteEffect_btn.Location = new System.Drawing.Point(691, 467);
+            this.DeleteEffect_btn.Name = "DeleteEffect_btn";
+            this.DeleteEffect_btn.Size = new System.Drawing.Size(283, 53);
+            this.DeleteEffect_btn.TabIndex = 9;
+            this.DeleteEffect_btn.Text = "Delete an Effect";
+            this.DeleteEffect_btn.UseVisualStyleBackColor = true;
+            // 
+            // LoadEffect_btn
+            // 
+            this.LoadEffect_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadEffect_btn.Location = new System.Drawing.Point(691, 408);
+            this.LoadEffect_btn.Name = "LoadEffect_btn";
+            this.LoadEffect_btn.Size = new System.Drawing.Size(283, 53);
+            this.LoadEffect_btn.TabIndex = 8;
+            this.LoadEffect_btn.Text = "Load an Existing Effect";
+            this.LoadEffect_btn.UseVisualStyleBackColor = true;
+            // 
+            // CreateEffect_btn
+            // 
+            this.CreateEffect_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateEffect_btn.Location = new System.Drawing.Point(691, 347);
+            this.CreateEffect_btn.Name = "CreateEffect_btn";
+            this.CreateEffect_btn.Size = new System.Drawing.Size(283, 55);
+            this.CreateEffect_btn.TabIndex = 7;
+            this.CreateEffect_btn.Text = "Create a New Effect";
+            this.CreateEffect_btn.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1002, 613);
+            this.Controls.Add(this.DeleteEffect_btn);
+            this.Controls.Add(this.LoadEffect_btn);
+            this.Controls.Add(this.CreateEffect_btn);
+            this.Controls.Add(this.DeleteCard_btn);
+            this.Controls.Add(this.LoadCard_btn);
+            this.Controls.Add(this.CreateCard_btn);
+            this.Controls.Add(this.DeleteSet_btn);
+            this.Controls.Add(this.LoadSet_btn);
+            this.Controls.Add(this.CreateSet_btn);
+            this.Controls.Add(this.CardMaker_logo);
+            this.Name = "MainForm";
+            this.Text = "CardMaker v1.0 by Att Rat Entertainment";
+            ((System.ComponentModel.ISupportInitialize)(this.CardMaker_logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CardMaker_logo;
+        private System.Windows.Forms.Button CreateSet_btn;
+        private System.Windows.Forms.Button LoadSet_btn;
+        private System.Windows.Forms.Button DeleteSet_btn;
+        private System.Windows.Forms.Button DeleteCard_btn;
+        private System.Windows.Forms.Button LoadCard_btn;
+        private System.Windows.Forms.Button CreateCard_btn;
+        private System.Windows.Forms.Button DeleteEffect_btn;
+        private System.Windows.Forms.Button LoadEffect_btn;
+        private System.Windows.Forms.Button CreateEffect_btn;
+    }
+}
+
