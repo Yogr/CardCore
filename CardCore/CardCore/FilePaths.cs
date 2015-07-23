d350f0a73e0d5131f412a9cf9ab085355cc1309a
Yogr <danh425@gmail.com>
2015-07-23 08:04:12 -0400

    July 23b
    some fixes

--- CardCore/CardCore/FilePaths.cs
+++ CardCore/CardCore/FilePaths.cs
@@ -9,7 +9,7 @@ namespace CardCore
 {
     static public class FilePaths
     {
-        static public string DATA_PATH = "../data/";
+        static public string DATA_PATH = "data/";
         static public string CARDS_PATH = "/cards/Cards.xml";
         static public string SETS_PATH = "/sets/Sets.xml";
         static public string EFFECTS_PATH = "/effects/Effects.xml";
